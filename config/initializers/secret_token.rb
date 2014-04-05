229925d7a9d40d176cc4c1fadaf1a211163c590f
Sergei Zinin <zinin@xakep.ru>
2014-04-05 15:50:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MavenlinkExample::Application.config.secret_key_base = '281051d229f93edd225595431ba4dfa6b3d4128c248b18a4dcd1d8cab8797cf1e9aaacfff3e694d8d041cd8a8de525a190305a26f27ee7eafb0204c80812ec6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MavenlinkExample::Application.config.session_store :cookie_store, key: '_mavenlink_example_session'
